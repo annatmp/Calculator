950c93acaed1400301e5a1684e0a8620b3534be5
anna <apillar3@gmail.com>
2020-06-20 10:20:15 +0200

    Basic Gui and logic

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,11 @@
 public class Calculator {
 
     public static void main(String[] argv) {
+        //this will later launch my application
 
+        View view = new View();
+        //int x =4;
+        //System.out.println(String.valueOf(x++));
+        //System.out.println(String.valueOf(x));
     }
 }
---
+++ src/Model.java
@@ -0,0 +1,30 @@
+public class Model {
+
+
+    private static double add(double ... y){
+        double solution = 0;
+        for (double add: y){
+            solution += add;
+        }
+        return solution;
+    }
+    private static double substract(double x, double ... y){
+        double substractor = add(y);
+        return x-substractor;
+    }
+    private static double multiply(double ...y) {
+        double solution = 1;
+        for (double factor:y) {
+            solution *= factor;
+        }
+        return solution;
+    }
+    private static double divide(double x,double ...y) {
+        double solution = x;
+        for (double factor:y) {
+            solution /= factor;
+        }
+        return solution;
+    }
+
+}
